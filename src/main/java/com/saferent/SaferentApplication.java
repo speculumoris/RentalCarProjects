cf1fb9f0fd5379d005100f8daa27ab5bf42ab1ce
speculumoris <sefa.byndr4@gmail.com>
2023-02-14 20:34:07 +0300

    firstDay

---
+++ src/main/java/com/saferent/SaferentApplication.java
@@ -0,0 +1,13 @@
+package com.saferent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaferentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaferentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saferent/SaferentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saferent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaferentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
